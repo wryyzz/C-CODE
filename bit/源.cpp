55fdb891599513bb0c40f70115342593c18911ec
bytezrj <1806632479@qq.com>
2022-10-31 20:45:16 +0800

    测试GitHub代码

---
+++ bit/源.cpp
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+////{
+////	char arr1[] = "abc";
+////	char arr2[] = { 'a', 'b', 'c','\0'};
+////
+////	printf("%s", arr1);
+////	printf("%s", arr2);
+////	return 0;
+//}
+//
+//{
+//	int input = 0;
+//    printf("你是不是sb（1/0)？");
+//    scanf("%d", &input);
+//    if (input == 1)
+//        printf("那么你是张吉森/n" ); 
+//    else
+//        printf("那么你是张正铎/n" );
+//
+//
+//
+//    
+//
+//	return 0;
+//}
+
+	
+//
+//}int main()
+//{
+//	int input = 0;
+//	printf("你好吗(1/0)？>:");
+//	scanf("%d", &input);
+//	if (input == 1)
+//		printf("好到飞起\n");
+//	else
+//		printf("难受到爆\n");
+//	return 0;
+//
+//int main()/*
+//{
+//	int line = 0;
+//	while (line < 3000)
+//	{
+//		printf("hh:%d\n", line);
+//		line++;
+//	}
+//       if (line == 3000)
+//	{	
+//	  printf("goood\n");
+//	}
+//	return 0;
+//}
